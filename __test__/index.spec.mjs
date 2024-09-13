2d5022bcfeaa5bffd39f4b1e43cb4a75940c59ec
starkfire <plpantoja49@gmail.com>
2024-09-13 14:34:55 +0800

    attempt version bump

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,11 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { sum, fibonacci } from '../index.js'
 
 test('sum from native', (t) => {
     t.is(sum(1, 2), 3)
+})
+
+test('fibonacci', (t) => {
+    t.is(fibonacci(5), 5)
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 #![deny(clippy::all)]
 
-#[macro_use]
-extern crate napi_derive;
+use napi_derive::napi;
 
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
+}
+
+#[napi]
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        1 | 2 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
